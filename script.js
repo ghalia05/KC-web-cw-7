3f5860563e759d2e9bbbf71739d3765d4de53005
ghalia05 <109863797+ghalia05@users.noreply.github.com>
2022-08-17 20:28:13 +0300

    Cw7

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
+function BMI (w,h) {
+let result = w / (h*h);
+return result
+}
+
+console.log(BMI(10,10))
+
+function status (bmi) {
+if (bmi < 18.5){ 
+    return " نقص بالوزن"
+} else if ( bmi >= 18.5 && bmi < 25 ){
+ return " وزنك صحي "
+} else if ( bmi >= 25){ 
+    return " لديك زياده في الوزن"
+}
+}
+
+
+function calculate () {
+
+let weight = document.getElementById(weight)
+	Value
+
+let height = document.getElementById(height) 
+value
+
+let bmi = BMI (weight,height)
+let desc = status (bmi) 
+
+let div = document.getElementById('divTag')
+div.innerText = bmi + " == " + desc
+}
+
 
